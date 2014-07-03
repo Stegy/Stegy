52093808cdd34614708755c4b54713e499c7a841
Joey Alvizo <josephalvizomendoza@gmail.com>
2014-07-03 16:34:21 -0500

    Started Block Utility Class
    For finding complexity and doing conjugations

---
+++ BlockUtility.cpp
@@ -0,0 +1,9 @@
+/*
+ * BlockUtility.cpp
+ *
+ *  Created on: Jul 3, 2014
+ *      Author: Joseph
+ */
+
+using namespace std;
+
---
+++ BlockUtility.h
@@ -0,0 +1,11 @@
+/*
+ * BlockUtility.h
+ *
+ * Created on: Jul 3, 2014
+ *      Author: Joseph
+ */
+
+#ifndef Block_Utility_H_
+#define Block_Utility_H_
+
+#endif /* Block_Utility_H_ */
